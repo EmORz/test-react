357e0ca50ca0154c3084828ebe74391f97b25fa3
Emo <eiv.razgrad@gmail.com>
2020-04-24 15:56:24 +0300

    simple add

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit ccccccxxxxxxxxxxxxxcccccc Emo <code>src/App.js</code> and save to reload.
+          Edit test Emo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
